65d3bd7360f8d6f61f2f45810e6b017a73acd215
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-01-18 17:24:16 +0800

    added home button

--- script.js
+++ script.js
@@ -14,8 +14,14 @@ const info = [
 const girl = document.querySelector("#girl")
 const main = document.querySelector(".main")
 const gallery = document.querySelector(".gallery")
+const home = document.querySelector(".home")
 
 girl.addEventListener('click', () => {
     gallery.style.display = "block"
     main.style.display = "none"
+})
+
+home.addEventListener('click', () => {
+    gallery.style.display = "none"
+    main.style.display = "flex"
 })
