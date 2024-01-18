24657121209a30ffdc534f6e1ab7750915a0da2b
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-01-18 17:10:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ const info = [
     {
         name: "Mira's Birthday",
         tag: "Birthday",
-        image: 
+        image: "none"
     }
 ]
+
+const girl = document.querySelector(".girl")
+const main = document.querySelector(".main")
+const gallery = document.querySelector(".gallery")
+
+girl.addEventListener('click', () => {
+    gallery.style.display = "block"
+    main.style.display = "none"
+    console.log("good")
+})
