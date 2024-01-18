7b64f9b05c40fbd7734c9619b4b25abe133aac7c
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-01-18 20:16:51 +0800

    update

--- script.js
+++ script.js
@@ -12,8 +12,10 @@ const info = [
 ]
 
 const girl = document.querySelector("#girl")
+const boy = document.querySelector("#boy")
 const main = document.querySelector(".main")
 const galleryOne = document.querySelector(".gallery-one")
+const galleryTwo = document.querySelector(".gallery-two")
 const home = document.querySelector(".home")
 
 girl.addEventListener('click', () => {
@@ -21,7 +23,13 @@ girl.addEventListener('click', () => {
     main.style.display = "none"
 })
 
+boy.addEventListener('click', () => {
+    galleryTwo.style.display = "block"
+    main.style.display = "none"
+})
+
 home.addEventListener('click', () => {
     galleryOne.style.display = "none"
+    galleryTwo.style.display = "none"
     main.style.display = "flex"
 })
