e24a7e1ef541bcf5e8102cf53a488cefdd45043f
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-07-30 20:25:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -107,6 +107,89 @@ const picturesGirl = [
     }
 ]
 
+const picturesBoy = [
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/1.jpg"
+    },
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/1.jpg"
+    },
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/1.jpg"
+    },
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/1.jpg"
+    },   
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/1.jpg"
+    },
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/2.jpg"
+    },
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/3.jpg"
+    },
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/4.jpg"
+    },
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/5.jpg"
+    },
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/6.jpg"
+    },
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/7.jpg"
+    },
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/8.jpg"
+    },
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/9.jpg"
+    },
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/10.jpg"
+    },
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/11.jpg"
+    },
+    {
+        name: "Mira's Birthday",
+        tag: "Birthday",
+        image: "/assets/12.jpg"
+    }
+]
+
 const girl = document.querySelector("#girl")
 const boy = document.querySelector("#boy")
 const main = document.querySelector(".main")
@@ -140,6 +223,41 @@ home.addEventListener('click', () => {
 const imageContainer = document.querySelector("#girl-images")
 
 picturesGirl.forEach(image => {
+    const galleryImages = document.createElement("img");
+    galleryImages.src = image.image;
+    galleryImages.style.cursor = "pointer"
+    imageContainer.appendChild(galleryImages)
+
+    const modal = document.createElement("div")
+    const modalImage = document.createElement("img");
+    modalImage.src = image.image;
+    modalImage.style.cursor = "pointer"
+    modal.appendChild(modalImage)
+    
+    
+    const close = document.createElement("span")
+    close.classList.add("close")
+    close.innerText = "X"
+    modal.appendChild(close)
+
+    galleryImages.addEventListener('click', () => {
+        close.style.display = "block"
+        modal.classList.add("modal")
+        modal.style.display = "flex"
+        modalImage.classList.add("modal_image")
+        imageContainer.appendChild(modal)
+
+        close.addEventListener('click', () => {
+            // modal.style.display = "none";
+            modal.classList.remove("modal")
+            modal.style.display = "none"
+            close.style.display = "none"
+        })
+    })
+})
+
+
+picturesBoy.forEach(image => {
     const galleryImages = document.createElement("img");
     galleryImages.src = image.image;
     galleryImages.style.cursor = "pointer"
