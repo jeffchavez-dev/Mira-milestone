cecc23492788684b7cdbeb11567677eabe042f85
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-07-30 20:27:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -111,82 +111,7 @@ const picturesBoy = [
     {
         name: "Mira's Birthday",
         tag: "Birthday",
-        image: "/assets/1.jpg"
-    },
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/1.jpg"
-    },
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/1.jpg"
-    },
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/1.jpg"
-    },   
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/1.jpg"
-    },
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/2.jpg"
-    },
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/3.jpg"
-    },
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/4.jpg"
-    },
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/5.jpg"
-    },
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/6.jpg"
-    },
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/7.jpg"
-    },
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/8.jpg"
-    },
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/9.jpg"
-    },
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/10.jpg"
-    },
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/11.jpg"
-    },
-    {
-        name: "Mira's Birthday",
-        tag: "Birthday",
-        image: "/assets/12.jpg"
+        image: "/assets/Kesed 1.jpg"
     }
 ]
 
@@ -195,6 +120,7 @@ const boy = document.querySelector("#boy")
 const main = document.querySelector(".main")
 const galleryOne = document.querySelector(".gallery-one")
 const galleryOneBG = document.querySelector(".gallery-one-bg")
+const galleryTwoBG = document.querySelector(".gallery-two-bg")
 const galleryTwo = document.querySelector(".gallery-two")
 const home = document.querySelector(".home")
 
@@ -209,6 +135,7 @@ boy.addEventListener('click', () => {
     galleryTwo.style.display = "block";
     main.style.display = "none";
     home.style.display = "block";
+    galleryTwoBG.style.display = "block"
 })
 
 home.addEventListener('click', () => {
