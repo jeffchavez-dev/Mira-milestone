4945df5741617ba2da840c2cccc548e4e87db370
Jeff Chavez <67730687+jeffchavez-dev@users.noreply.github.com>
2024-08-01 09:50:25 +0800

    updated

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 
-lightGallery(document.getElementById('lightgallery'), {
-    plugins: [lgZoom, lgThumbnail],
-    licenseKey: 'your_license_key',
-    speed: 500,
-    // ... other settings
-});
-
 
 const picturesGirl = [
     {
